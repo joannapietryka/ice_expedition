be4dd19ad1c6e6f80f10f3bed612b54c6a873195
Joanna Pietryka <joannapietryka@MacBook-Pro-Joanna.local>
2018-06-13 18:01:23 +0200

    files

--- script/script.js
+++ script/script.js
@@ -10,7 +10,29 @@ $(function () {
 			new ScrollMagic.Scene({
 					triggerElement: slides[i]
 				})
-				.setPin(slides[i])
+                .setPin(slides[i])
 				.addTo(controller);
 		}
+
+		var dots = document.querySelectorAll('.dot');
+    for (let i = 0; i<dots.length; i++){
+      new ScrollMagic.Scene({
+        triggerElement: slides[i],
+		reverse: true
+      })
+      .setClassToggle(dots[i], 'selected')
+      .addTo(controller); 
+    };
+
+	var animate = document.querySelectorAll('section');
+	
+	
+	for (var i=0; i<animate.length; i++) {
+			new ScrollMagic.Scene({
+					triggerElement: animate[i]
+				})
+				.setClassToggle(animate[i], "animate")
+				.addTo(controller);	
+			};
+
 	});
