95de5fa6e7ade2746389cc28eeb7a40a7de6f2eb
Joanna Pietryka <joannapietryka@MacBook-Pro-Joanna.local>
2018-07-25 17:28:57 +0200

    update

--- script/script.js
+++ script/script.js
@@ -1,38 +1,37 @@
-$(function () { 
-	var controller = new ScrollMagic.Controller({
-		globalSceneOptions: {
-          triggerHook: 'onLeave'
-			}
-		});
+$(function() {
+  var controller = new ScrollMagic.Controller({
+    globalSceneOptions: {
+      triggerHook: "onLeave"
+    }
+  });
 
-    var slides = $("section");
-		for (var i=0; i<slides.length; i++) {
-			new ScrollMagic.Scene({
-					triggerElement: slides[i]
-				})
-                .setPin(slides[i])
-				.addTo(controller);
-		}
+  var slides = $("section");
+  for (var i = 0; i < slides.length; i++) {
+    new ScrollMagic.Scene({
+      triggerElement: slides[i]
+    })
+      .setPin(slides[i])
+      .addTo(controller);
+  }
 
-		var dots = document.querySelectorAll('.dot');
-    for (let i = 0; i<dots.length; i++){
-      new ScrollMagic.Scene({
-        triggerElement: slides[i],
-		reverse: true
-      })
-      .setClassToggle(dots[i], 'selected')
-      .addTo(controller); 
-    };
+  var dots = document.querySelectorAll(".dot");
+  for (let i = 0; i < dots.length; i++) {
+    new ScrollMagic.Scene({
+      triggerElement: slides[i],
+      reverse: true
+    })
+      .setClassToggle(dots[i], "selected")
+      .addTo(controller);
+  }
 
-	var animate = document.querySelectorAll('section');
-	
-	
-	for (var i=0; i<animate.length; i++) {
-			new ScrollMagic.Scene({
-					triggerElement: animate[i]
-				})
-				.setClassToggle(animate[i], "animate")
-				.addTo(controller);	
-			};
+  var animate = document.querySelectorAll("section");
 
-	});
+  for (var i = 0; i < animate.length; i++) {
+    new ScrollMagic.Scene({
+      triggerElement: animate[i]
+    })
+      .setClassToggle(animate[i], "animate")
+      .addTo(controller);
+  }
+  $(".offer-box").slideUp(slow);
+});
